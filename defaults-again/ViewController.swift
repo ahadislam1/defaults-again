3c2a0e55e5c2860002ab20f02b671c73f5b6d079
Ahad Islam <ahad.islam624@gmail.com>
2019-12-23 11:05:21 -0500

    added code for functionality

--- defaults-again/ViewController.swift
+++ defaults-again/ViewController.swift
@@ -10,11 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet var allLabels: [UILabel]!
+    @IBOutlet weak var textField: UITextField!
+    
+    var fontSize: Double = 10 {
+        didSet {
+            allLabels.forEach { $0.font = $0.font.withSize(CGFloat(fontSize)) }
+        }
+    }
+    
+    var uppercaseText = false {
+        didSet {
+            switch uppercaseText {
+            case true:
+                allLabels.forEach { $0.text = $0.text?.uppercased() }
+            case false:
+                allLabels.forEach { $0.text = $0.text?.lowercased() }
+            }
+        }
+    }
+    
+    var username = "" {
+        didSet {
+            welcomeLabel.text = "Welcome " + username + "!"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textField.delegate = self
+        
     }
 
+    @IBAction func updateFontSize(_ sender: UISlider) {
+        fontSize = Double(sender.value)
+    }
+    
+    @IBAction func updateUppercaseText(_ sender: UISwitch) {
+        uppercaseText = sender.isOn
+    }
+    
+    
+}
 
+extension ViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if let text = textField.text,
+            let textRange = Range(range, in: text) {
+            let updatedText = text.replacingCharacters(in: textRange, with: string)
+            username = updatedText
+        }
+        print(username)
+        return true
+    }
 }
 
